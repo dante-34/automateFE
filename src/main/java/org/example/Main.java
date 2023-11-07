3644989a31deffe138c28eee5a4a38651cc19751
dante-34 <dan.tenne@gmail.com>
2023-11-07 18:14:51 +0200

    working version, producing JSON saved as TXT file. Test mode is sampling 3 of the contacts, and not all are scraped.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,8 +14,8 @@ import java.util.List;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
-
+    public static void main(String[] args)  {
+// throws InterruptedException ??
         String targetJsonPath = "outputLinkedinContacts.txt";
         String credentialsLinkedinUser = "YOUR-USER-NAME";
         String credentialsLinkedinPassword = "YOUR-PASSWORD";
